859c3e9644c4287ebbd61c51ce9639919fe0ae3e
skwibblez <watashinoasian@gmail.com>
2020-08-30 22:47:01 -0700

    chat test

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+// io.on('connection', (socket) => {
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//   });
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+
+
+var PORT = 2222
+http.listen(PORT, () => {
+  console.log('listening on *:2222');
+});
