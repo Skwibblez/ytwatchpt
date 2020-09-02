b88434e15a59af0b78ef0354080b7eea83225321
skwibblez <watashinoasian@gmail.com>
2020-09-02 03:51:14 -0700

    removed main.js files

--- main.js
+++
@@ -1,9 +0,0 @@
-function echo(){
-  console.log("hey this worked")
-}
-function deleteRow(obj) {
-    //Test how sockets work
-    var socket = io();
-    var index = obj.parentNode.parentNode.rowIndex;
-    socket.emit('delete row', index);
-}
